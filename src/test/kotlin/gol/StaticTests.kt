91e2272c57e8c66b9dd52c0be46466c8c44d4341
Allison Knauss <pknauss@nerdery.com>
2016-12-21 13:47:05 -0600

    Add a minimal sample test

---
+++ src/test/kotlin/gol/StaticTests.kt
@@ -0,0 +1,17 @@
+package gol
+
+import org.junit.Test
+import org.junit.Assert.*
+
+/** A basic test example
+ *
+ * Created by aknauss on 12/21/16.
+ */
+
+class StaticTests {
+
+    @Test
+    fun simple() {
+        assertEquals(4, 2 * 2)
+    }
+}
